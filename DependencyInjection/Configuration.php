db6b470aab0d618dc3fd27e71ac9dddf85a5224d
blacklizer <blacklizer@proton.me>
2025-02-17 19:17:02 +0300

    SF7 compatible fix

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -14,7 +14,7 @@ class Configuration implements ConfigurationInterface
     /**
      * {@inheritdoc}
      */
-    public function getConfigTreeBuilder()
+    public function getConfigTreeBuilder(): TreeBuilder
     {
         $twigFunctionsNormalizer = function ($list) {
             $normalized = [];
